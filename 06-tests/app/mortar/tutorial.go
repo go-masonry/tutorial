9f6e5f6251366314c2ca60631fbf88a06379e75f
Tal Gendler <tal.gendler@gmail.com>
2020-09-04 15:05:38 +0300

    06 part upgraded to grpc-gateway v2

--- 06-tests/app/mortar/tutorial.go
+++ 06-tests/app/mortar/tutorial.go
@@ -10,7 +10,7 @@ import (
 	"github.com/go-masonry/tutorial/06-tests/app/data"
 	"github.com/go-masonry/tutorial/06-tests/app/services"
 	"github.com/go-masonry/tutorial/06-tests/app/validations"
-	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"go.uber.org/fx"
 	"google.golang.org/grpc"
 )
